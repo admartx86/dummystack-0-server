715c68b41a104701fbdf1c7cc91948efc6a44690
Adam Martinez <12470793+mart3318@users.noreply.github.com>
2024-02-21 15:33:04 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const articleRouter = require("./route");
+
+dotenv.config();
+
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log('Node.js listening...' + port)
+})
+app.use("articles", articleRouter);
